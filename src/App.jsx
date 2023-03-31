ddfe50e31c8683e3277cd7a480ff1af9acef7528
SylvainAlx <sylval49@gmail.com>
2023-03-31 12:15:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./layouts/Header";
+import Home from "./Pages/Home";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/layouts/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Sylvain ALEXANDRE</h1>
+    </header>
+  );
+};
+
+export default Header;
