63c1c2ff30b69c6a449cbf5971cb4fd8040ebe96
SylvainAlx <sylval49@gmail.com>
2023-04-12 21:16:07 +0200

    bridge

--- src/components/rooms/Bridge.jsx
+++ src/components/rooms/Bridge.jsx
@@ -1,12 +1,37 @@
 import RoomHeader from "./RoomHeader";
+import Button from "../Button";
+import "../../assets/scss/room.scss";
 
 const Bridge = () => {
   return (
-    <div className="room">
+    <div className="room bridge">
       <RoomHeader
+        image="src/assets/images/bridge.jpg"
         roomName="Salle de contrôle"
         description="Parcours et ambitions"
       />
+      <div className="sections">
+        <section>
+          <h4>d'où je viens</h4>
+          <ul>
+            <li>2023 - BAC+2 Developpeur web full-stack</li>
+            <li>2017-2022 - Assistant d'éducation</li>
+            <li>2017-2019 - Photographe freelance</li>
+            <li>2011-2016 - Technicien Canal+</li>
+            <li>2010 - BAC+2 Audiovisuel</li>
+          </ul>
+        </section>
+        <section>
+          <h4>d'où je viens</h4>
+          <ul>
+            <li>développeur web fullstack React/Node.js</li>
+            <li>developpeur WordPress</li>
+            <li>developpeur Front-End</li>
+            <li>intégrateur web</li>
+          </ul>
+        </section>
+      </div>
+      <Button text={"télécharger mon CV"} />
     </div>
   );
 };
--- src/components/rooms/RoomHeader.jsx
+++ src/components/rooms/RoomHeader.jsx
@@ -1,8 +1,13 @@
 const RoomHeader = (props) => {
   return (
     <header>
-      <h5>{props.roomName}</h5>
-      <h3>{props.description}</h3>
+      <div className="roomImage">
+        <img src={props.image} />
+      </div>
+      <div>
+        <h5>{props.roomName}</h5>
+        <h3>{props.description}</h3>
+      </div>
     </header>
   );
 };
