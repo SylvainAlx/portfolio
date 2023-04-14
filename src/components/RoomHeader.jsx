94685faf9c8703078bc43deadecc4121282d8e0e
SylvainAlx <sylval49@gmail.com>
2023-04-14 17:36:56 +0200

    deploy

--- src/components/RoomHeader.jsx
+++ src/components/RoomHeader.jsx
@@ -3,7 +3,7 @@ const RoomHeader = (props) => {
     <header>
       <h5>{props.roomName}</h5>
       <div className="roomImage">
-        <img src={props.image} />
+        <img src={`.${props.image}`} />
       </div>
       <h3>{props.description}</h3>
     </header>
