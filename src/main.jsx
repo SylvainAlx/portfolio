8ad7ede34590c588810c3a892a1a83909f1c4622
Sylvain Alexandre <sylvainalx.dev@gmail.com>
2024-11-05 20:03:13 +0100

    ajout Google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-XZ54H705D7");
+ReactGA.send("pageview");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
